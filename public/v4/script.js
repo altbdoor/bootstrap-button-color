2c925d32f6eba57f58e0de2ae479a505ba556917
altbdoor <lancersupraskyline@gmail.com>
2018-10-27 18:16:40 +0800

    update sass js and rawgit sunset

--- public/v4/script.js
+++ public/v4/script.js
@@ -1,8 +1,8 @@
 (function (Sass) {
-	Sass.setWorkerUrl('public/v4/sass.worker.min.js')
-	
+	Sass.setWorkerUrl('public/v4/sass.worker.js')
+
 	function getDepsOnVersion (version) {
-		var depUrl = 'https://cdn.rawgit.com/twbs/bootstrap/' + version + '/scss/'
+		var depUrl = 'https://cdn.jsdelivr.net/gh/twbs/bootstrap@' + version + '/scss/'
 		var deps = [
 			'_functions.scss',
 			'_variables.scss',
@@ -13,71 +13,71 @@
 		]
 		var depPromises = []
 		var depResults = {}
-		
+
 		for (var i=0; i<deps.length; i++) {
 			var depPromise = $.ajax({
 				url: depUrl + deps[i],
 				dataType: 'text',
 				type: 'GET',
 			})
-			
+
 			function depPromiseCallback (depIndex) {
 				return function (data) {
 					depResults[depIndex] = data
 				}
 			}
-			
+
 			depPromise.then(depPromiseCallback(deps[i]))
 			depPromises.push(depPromise)
 		}
-		
+
 		return $.when.apply($, depPromises).then(function () {
 			var depContent = ''
-			
+
 			for (var i=0; i<deps.length; i++) {
 				depContent += '\n' + depResults[deps[i]]
 			}
-			
+
 			return $.when(depContent)
 		})
 	}
-	
+
 	$('#generator-form').on('submit', function (e) {
 		e.preventDefault()
-		
+
 		var versionVal = $('#form-version').val()
 		var colorVal = $('#form-color').val()
 		var classVal = $('#form-class').val()
 		classVal = slugify(classVal)
-		
+
 		var processModal = $('#process-modal')
 		$(processModal).modal('show')
-		
+
 		var scssContent = '\n.btn-custom { @include button-variant(#' + colorVal + ', #' + colorVal + '); }'
 		if ($(this).find('[name="form-outline"]').filter(':checked').val() == 'yes') {
 			scssContent = '\n.btn-custom { @include button-outline-variant(#' + colorVal + '); }'
 		}
-		
+
 		var promise = getDepsOnVersion(versionVal)
 		promise.then(function (depContent) {
 			depContent += scssContent
-			
+
 			var sassInstance = new Sass()
-			
+
 			sassInstance.compile(depContent, {
 				style: Sass.style.expanded,
 			}, function (result) {
 				$('#output-text').val(result.text.replace(/\.btn-custom/g, '.btn-' + classVal))
 				$('#output-style').html(result.text)
 				$(processModal).modal('hide')
-				
+
 				sassInstance.destroy()
 			})
 		})
 	}).on('reset', function () {
 		$(this).find('[name="form-outline"][value="no"]').parent().trigger('click')
 	})
-	
+
 	// https://gist.github.com/mathewbyrne/1280286
 	function slugify (text) {
 		return text.toString().toLowerCase()
@@ -87,5 +87,5 @@
 			.replace(/^-+/, '')             // Trim - from start of text
 			.replace(/-+$/, '');            // Trim - from end of text
 	}
-	
+
 })(Sass)
